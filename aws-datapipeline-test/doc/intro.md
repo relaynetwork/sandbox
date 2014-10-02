f4a589fb4bdf7245d0369a1580d78aa15e62c3e7
Rich and Nick <kburton@relaynetwork.com>
2014-10-02 09:21:33 -0400

    sure

---
+++ aws-datapipeline-test/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to aws-datapipeline-test
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ aws-datapipeline-test/test/aws_datapipeline_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns aws-datapipeline-test.core-test
+  (:require [clojure.test :refer :all]
+            [aws-datapipeline-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
